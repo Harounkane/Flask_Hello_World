59b5861164426cc90ad27b0469df99381e445228
Harounkane <165789938+Harounkane@users.noreply.github.com>
2024-04-11 12:47:06 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,7 +19,12 @@ def MaPremiereAPI():
 
 @app.route('/carre/<int:val_user>')
 def carre(val_user):
-    return "<h2>Le carré de votre valeur est : </h2>" + str(val_user * val_user)                                                                                                               
+    return "<h2>Le carré de votre valeur est : </h2>" + str(val_user * val_user)    
+  
+@app.route('/somme/<int:valeur1>/<int:valeur2>')
+def somme(valeur1,valeur2):
+    return "<h2>La somme est : </h2>" + str(valeur1 + valeur2)
+  
 if __name__ == "__main__":
   app.run(debug=True)
 
