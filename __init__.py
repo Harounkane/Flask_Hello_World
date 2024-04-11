d7acd6cd35fb7194091339f3f62cf5f377dab3b2
Harounkane <165789938+Harounkane@users.noreply.github.com>
2024-04-11 12:25:24 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@ def exercices():
 def MaPremiereAPI():
     return render_template('contact.html')
 
-@app.route('/calcul_carre/<int:val_user>')
+@app.route('/carre/<int:val_user>')
 def carre(val_user):
     return "<h2>Le carré de votre valeur est : </h2>" + str(val_user * val_user)
                                                                                                                
